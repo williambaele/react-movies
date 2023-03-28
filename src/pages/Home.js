1f7d226c1175407bb5c6d447f77f1175ae51586a
William Baele <baelewilliam1999@gmail.com>
2023-03-28 13:19:26 +0200

    added axios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import axios from "axios";
+
+// import Card from '../components/Card';
 
 const Home = () => {
   return (
-    <div className="min-h-screen bg-orange-200">
-        <h1>Hi</h1>
+    <div className="min-h-screen bg-[#1c2541] py-10">
+      <div className="container mx-auto flex flex-col gap-4">
+        <div>
+          <h1 className="text-4xl font-medium text-center text-gray-100">React Movies</h1>
+        </div>
+        <div className="w-1/3 self-center">
+          <form>
+            <div>
+              <div className="flex rounded-md overflow-hidden w-full">
+                <input type="text" className="w-full rounded-md rounded-r-none outline-none pl-4" placeholder='Your next movie'/>
+                <button className="bg-indigo-600 text-white px-6 text-lg font-semibold py-2 rounded-r-md outline-none">Go</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
