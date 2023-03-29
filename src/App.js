a03ddad133a1e78511d2d275622f95ad6acf68d7
William Baele <baelewilliam1999@gmail.com>
2023-03-29 09:49:43 +0200

    added conditional header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Home from "./pages/Home";
 import "./styles/index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Liked from "./pages/Liked";
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />}/>
+      <Route path="liked" element={<Liked />}/>
 
     </Routes>
   </BrowserRouter>
---
+++ src/pages/Liked.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Liked = () => {
+  return (
+    <div className="min-h-screen bg-[#1c2541]">
+      <Header/>
+      <div className="container mx-auto flex flex-col md:gap-4 py-4">
+      </div>
+    </div>
+  );
+};
+
+export default Liked;
